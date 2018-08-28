91e9c76114f1aa9508d5aa126b5f1b77d258d775
Onray <mcintyrehh@gmail.com>
2018-08-28 19:55:10 -0400

    test commit

--- server.js
+++ server.js
@@ -17,29 +17,76 @@ var reservations = [
     }
 ];
 
-app.get("/", function(req, res) {
+app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
 });
 
-app.get("/reserve", function(req, res) {
+app.get("/reserve", function (req, res) {
     res.sendFile(path.join(__dirname, "reserve.html"));
 });
 
-app.get("/api/tables", function(req, res) {
+app.get("/api/tables", function (req, res) {
     res.json(reservations.slice(0, 4));
 });
 
-app.get("/api/waitlist", function(req, res) {
+app.get("/api/waitlist", function (req, res) {
     res.json(reservations.slice(5));
 });
-app.post("/api/tables", function(req, res) {
-var newRes = req.body;
-  console.log(newRes);
-  reservations.push(newRes);
-  res.json(newRes);
+app.post("/api/tables", function (req, res) {
+    var newRes = req.body;
+    console.log(newRes);
+    reservations.push(newRes);
+    res.json(newRes);
 })
 
 //starts listening
-app.listen(PORT, function() {
+app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
 });
+
+{/* <script type="text/javascript"> */ }
+
+$("#add-btn").on("click", function (event) {
+    event.preventDefault()
+
+    var newRes = {
+        id: $("#id").val().trim(),
+        name: $("#name").val().trim(),
+        phone: $("#phone").val().trim(),
+        email: $("#email").val().trim()
+    };
+
+    // Question: What does this code do??
+    $.post("/api/tables", newRes)
+        .then(function (data) {
+            console.log(data);
+            console.log("Reservation Added");
+        });
+
+});
+
+// </script >
+// <script>
+$.get("/api/tables", function(data) {
+    for (var i = 0; i<data.length; i++) {
+        var res = data[i];
+        $("#table").append(createCard(res));
+    }
+})
+function createCard(res) {
+
+    var card = $("<div>")
+    card.append($("<div>").text("Name: " + res.name));
+    card.append($("<div>").text("Phone Number: " + res.phone));
+    card.append($("<div>").text("Email: " + res.email));
+    card.append($("<div>").text("ID: " + res.id));
+    return card;
+}
+$.get("/api/waitlist", function(data) {
+    for (var i = 0; i<data.length; i++) {
+        var res = data[i];
+        $("#waitlist").append(createCard(res));
+    }
+})
+//</script
+
